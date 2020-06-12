cfbf93b79357ba71fb00230c9423edd0cce1a413
Steven <mtbsteven@gmail.com>
2020-06-12 10:19:26 -0700

    Initial Commit

---
+++ Tree Traversal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tree Traversal
+//
+//  Created by Steven Wright on 6/12/20.
+//  Copyright © 2020 Steven Wright. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tree TraversalTests/Tree_TraversalTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tree_TraversalTests.swift
+//  Tree TraversalTests
+//
+//  Created by Steven Wright on 6/12/20.
+//  Copyright © 2020 Steven Wright. All rights reserved.
+//
+
+import XCTest
+@testable import Tree_Traversal
+
+class Tree_TraversalTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
